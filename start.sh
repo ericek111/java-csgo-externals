363e264e154993058190e9a2c5b6dc8f271f2c3f
ericek111 <eb.skola@gmail.com>
2017-03-20 23:00:04 +0100

    Fixed condition in the start script

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-if [ "$(whoami)" == 'root' ]; then
+if [ "$(whoami)" != 'root' ]; then
 	echo "ERROR: You *must* run this script as root!"
+	exit 1;
 fi
 ABSPATH=`pwd`
 java -cp "$ABSPATH/lib/*:$ABSPATH/CSGOExternals.jar" me.lixko.csgoexternals.Main
