942e4816f6eac84597c889a6ea447a903a9189d6
ericek111 <eb.skola@gmail.com>
2017-04-21 22:44:49 +0200

    Fixed GetClassName

--- src/me/lixko/csgoexternals/util/MemoryUtils.java
+++ src/me/lixko/csgoexternals/util/MemoryUtils.java
@@ -8,7 +8,9 @@ public class MemoryUtils {
 		long vtable = Engine.clientModule().readLong(entityaddr + 8);
 		long fn = Engine.clientModule().readLong(vtable - 8);
 		long cls = Engine.clientModule().readLong(fn + 8);
-		return Engine.clientModule().readString(cls, 64);
+		String classname = Engine.clientModule().readString(cls, 64);
+		classname = classname.substring(0, classname.indexOf(' '));
+		return classname;
 	}
 	
 }
