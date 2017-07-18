468da5855b57c39598b98b2bd049163c0f3e57a1
ericek111 <eb.skola@gmail.com>
2017-07-19 00:28:51 +0200

    fields in GlowObjectDefinition

--- src/me/lixko/csgoexternals/structs/GlowObjectDefinition.java
+++ src/me/lixko/csgoexternals/structs/GlowObjectDefinition.java
@@ -9,15 +9,18 @@ public class GlowObjectDefinition extends MemStruct {
 	public final StructField m_flGlowGreen = new StructField(this, Float.BYTES);
 	public final StructField m_flGlowBlue = new StructField(this, Float.BYTES);
 	public final StructField m_flGlowAlpha = new StructField(this, Float.BYTES);
-	public final StructField unk0 = new StructField(this, 16);
+	public final StructField unk0 = new StructField(this, 8);
+	public final StructField m_flBloomAmount = new StructField(this, Float.BYTES);
+	public final StructField localplayeriszeropoint3 = new StructField(this, Float.BYTES);
 	public final StructField m_bRenderWhenOccluded = new StructField(this, 1);
 	public final StructField m_bRenderWhenUnoccluded = new StructField(this, 1);
 	public final StructField m_bFullBloomRender = new StructField(this, 1);
-	public final StructField unk1 = new StructField(this, 1);
-	public final StructField m_nFullBloomStencilTestValue = new StructField(this, Integer.BYTES);
+	public final StructField unk2 = new StructField(this, 1);
+	public final StructField m_nGlowStyle = new StructField(this, Integer.BYTES);
+	public final StructField iUnk = new StructField(this, Integer.BYTES);
 	public final StructField m_nSplitScreenSlot = new StructField(this, Integer.BYTES);
 	public final StructField m_nNextFreeSlot = new StructField(this, Integer.BYTES);
-	public final StructField unk2 = new StructField(this, 8);
+	public final StructField unk3 = new StructField(this, 4);
 
 	static final int END_OF_FREE_LIST = -1;
 	static final int ENTRY_IN_USE = -2;
