2dcda6ba976b4ae9f25b8aec725c1a3e45810424
ericek111 <eb.skola@gmail.com>
2017-07-19 00:27:31 +0200

    ProfilerUtil

---
+++ src/me/lixko/csgoexternals/util/ProfilerUtil.java
@@ -0,0 +1,27 @@
+package me.lixko.csgoexternals.util;
+
+public class ProfilerUtil {
+	private static final ThreadLocal<Long> firstnanos = ThreadLocal.withInitial(() -> new Long(0));
+	private static final ThreadLocal<Long> lastnanos = ThreadLocal.withInitial(() -> new Long(0));
+
+	public static void start() {
+		firstnanos.set(System.nanoTime());
+	}
+
+	public static void measure(String text) {
+		if (firstnanos.get() == 0) {
+			firstnanos.set(System.nanoTime());
+			lastnanos.set(System.nanoTime());
+			System.out.println("[PROFILER] Started measurement.");
+			return;
+		}
+
+		long elapsed = System.nanoTime() - firstnanos.get();
+		long diff = System.nanoTime() - lastnanos.get();
+
+		System.out.format("[PROFILER] [%d] %d: %s\n", elapsed, diff, text);
+
+		lastnanos.set(System.nanoTime());
+
+	}
+}
