e3a3cc58acbe6bc0e4364adc74ee26865e3a7824
ericek111 <eb.skola@gmail.com>
2017-08-19 05:19:42 +0200

    Changed invalid import.

--- src/me/lixko/csgoexternals/modules/Bunnyhop.java
+++ src/me/lixko/csgoexternals/modules/Bunnyhop.java
@@ -3,7 +3,7 @@ package me.lixko.csgoexternals.modules;
 import me.lixko.csgoexternals.Client;
 import me.lixko.csgoexternals.Engine;
 import me.lixko.csgoexternals.offsets.Offsets;
-import me.lixko.csgoexternals.util.Flags;
+import me.lixko.csgoexternals.util.Const;
 
 public class Bunnyhop extends Module {
 
@@ -19,9 +19,9 @@ public class Bunnyhop extends Module {
 					if (Engine.clientModule().readInt(Offsets.input.alt1) == 5) {
 						long m_fFlags = Engine.clientModule().readLong(Offsets.m_dwLocalPlayer + Offsets.m_fFlags);
 						// TODO: Add randomization settings.
-						if (false && (m_fFlags & Flags.FL_ONGROUND) > 0)
+						if (false && (m_fFlags & Const.FL_ONGROUND) > 0)
 							Engine.clientModule().writeInt(Offsets.input.jump, 6);
-						else if ((m_fFlags & Flags.FL_ONGROUND) > 0) {
+						else if ((m_fFlags & Const.FL_ONGROUND) > 0) {
 							Thread.sleep(5 + (int) (Math.random() * 15));
 							Engine.clientModule().writeInt(Offsets.input.jump, 5);
 							Thread.sleep(15 + (int) (Math.random() * 100));
