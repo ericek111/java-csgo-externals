28cdf0ff99aac40b70fb749c4e90887b702ba3c4
ericek111 <erik.brocko@letemsvetemapplem.eu>
2020-12-02 01:36:04 +0100

    PatternScanner with proper masking

--- src/me/lixko/csgoexternals/offsets/PatternScanner.java
+++ src/me/lixko/csgoexternals/offsets/PatternScanner.java
@@ -16,6 +16,8 @@
 
 package me.lixko.csgoexternals.offsets;
 
+import java.util.Arrays;
+
 import com.github.jonatino.misc.Cacheable;
 import com.github.jonatino.process.Module;
 
@@ -26,35 +28,44 @@ public final class PatternScanner {
 	public static final int READ = 1, SUBTRACT = 2;
 
 	public static long getAddressForPattern(Module module, int pattern_offset, int address_offset, int flags, String className) {
-		return getAddressForPattern(module, pattern_offset, address_offset, flags, className.getBytes());
-	}
-
-	public static long getAddressForPattern(Module module, int pattern_offset, int address_offset, int flags, long value) {
-		return getAddressForPattern(module, pattern_offset, address_offset, flags, toByteArray(value));
-	}
-
-	public static long getAddressForPattern(Module module, int pattern_offset, int address_offset, int flags, int value) {
-		return getAddressForPattern(module, pattern_offset, address_offset, flags, toByteArray(value));
+		return getAddressForPattern(module, pattern_offset, address_offset, flags, null, className.getBytes());
 	}
 
 	public static long getAddressForPattern(Module module, int pattern_offset, int address_offset, int flags, int... values) {
-		return getAddressForPattern(module, pattern_offset, address_offset, flags, toByteArray(values));
+		return getAddressForPattern(module, pattern_offset, address_offset, flags, null, toByteArray(values));
 	}
 
+	/**
+	 * Match an IDA-style signature (EF F3 ?? ?? 00 00).
+	 * 
+	 * @param module
+	 * @param sigstr
+	 * @return
+	 */
 	public static long getAddressForPattern(Module module, String sigstr) {
-		return getAddressForPattern(module, 0, 0, 0, hexStringToByteArray(sigstr));
+		return getAddressForPattern(module, 0, 0, 0, hexStringToMask(sigstr), hexStringToByteArray(sigstr));
 	}
-
-	public static long getAddressForPattern(Module module, long values) {
-		byte[] barr = toByteArray(values);
-		System.out.println(StringFormat.hex(barr));
-		return getAddressForPattern(module, 0, 0, 0, barr);
-	}
-
-	public static long getAddressForPattern(Module module, int pattern_offset, int address_offset, int flags, byte... values) {
+	
+	/**
+	 * Find a signature in the memory space of the provided Module.
+	 * 
+	 * @param module
+	 * @param pattern_offset
+	 * @param address_offset
+	 * @param flags
+	 * @param mask If null, values are matched only if they're truly equal. Only 0xff/0x00 supported at the moment.
+	 * @param values Bytes to match against, must align with mask.
+	 * @return
+	 */
+	public static long getAddressForPattern(Module module, int pattern_offset, int address_offset, int flags, byte[] mask, byte[] values) {
+		if (mask == null) {
+			mask = new byte[values.length];
+			Arrays.fill(mask, (byte) 0xff);
+		}
+		
 		long off = module.size() - values.length;
 		for (long i = 0; i < off; i++) {
-			if (checkMask(module, i, values)) {
+			if (checkMask(module, i, mask, values)) {
 				i += module.start() + pattern_offset;
 				if ((flags & READ) == READ) {
 					i = module.process().readLong(i);
@@ -68,21 +79,13 @@ public final class PatternScanner {
 		throw new IllegalStateException("Can not find offset inside of " + module.name() + " with pattern " + StringFormat.hex(values)); // + Arrays.toString(values));
 	}
 
-	private static boolean checkMask(Module module, long offset, byte[] pMask) {
-		for (int i = 0; i < pMask.length; i++) {
-			if (pMask[i] != 0x0 && (pMask[i] != module.data().getByte(offset + i))) {
+	private static boolean checkMask(Module module, long offset, byte[] mask, byte[] data) {
+		for (int i = 0; i < data.length; i++) {
+			if (mask[i] != 0x00 && (data[i] != module.data().getByte(offset + i))) {
 				return false;
 			}
 		}
 		return true;
-	}
-
-	private static byte[] toByteArray(int value) {
-		return new byte[] { (byte) value, (byte) (value >> 8), (byte) (value >> 16), (byte) (value >> 24) };
-	}
-
-	private static byte[] toByteArray(long value) {
-		return new byte[] { (byte) value, (byte) (value >> 8), (byte) (value >> 16), (byte) (value >> 24), (byte) (value >> 32), (byte) (value >> 40), (byte) (value >> 48), (byte) (value >> 56) };
 	}
 
 	private static byte[] toByteArray(int... value) {
@@ -94,7 +97,7 @@ public final class PatternScanner {
 	}
 
 	public static byte[] hexStringToByteArray(String str) {
-		String s = str.replaceAll("\\s+", "").replace("??", "00");
+		String s = str.replaceAll("\\s+", "").replace("??", "00").toUpperCase();
 		int len = s.length();
 		byte[] data = new byte[len / 2];
 		for (int i = 0; i < len; i += 2) {
@@ -102,5 +105,19 @@ public final class PatternScanner {
 		}
 		return data;
 	}
-
+	
+	public static byte[] hexStringToMask(String str) {
+		String s = str.replaceAll("\\s+", "").toUpperCase();
+		int len = s.length();
+		byte[] data = new byte[len / 2];
+		for (int i = 0; i < len; i += 2) {
+			if (s.charAt(i) == '?' && s.charAt(i + 1) == '?') {
+				data[i / 2] = (byte) 0x00;
+			} else {
+				data[i / 2] = (byte) 0xff;
+			}
+		}
+		return data;
+	}
+	
 }
